5cc4d653c7171c8dbda4b59f6768229adb0007b5
Jeloves <anjelovaleraecube@icloud.com>
2023-11-02 21:47:35 -0400

    Kept working on data model.

--- data_model/classes.js
+++ data_model/classes.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
+import { uuid } from 'uuidv4';
 
 class User {
     constructor(username,password,email){
@@ -13,24 +14,105 @@ class User {
     }
 }
 
-class Budget {
-    constructor(name,dateCreated){
+class Budget {    
+    constructor(name,yearCreated,monthCreated,dayCreated){
         this.id = uuidv4();
         this.name = name;
-        this.dateCreated = dateCreated;
-        this.categories = ['Uncategorized'];
+        this.yearCreated = yearCreated;
+        this.monthCreated = monthCreated;
+        this.dayCreated = dayCreated;
+        this.accounts = [];
+        this.groups = [];
+        this.categories = [];
+        this.balances = [];
+        this.payees = [];
+        this.transactions = [];
     }
-    makeAccount(name,balance,currency) {
-        
+    addAccount(name,balance,currency) {
+        this.accounts.push(new Account(name,balance,currency));
+        this.accounts.sort((a1, a2) => (a1.name > a2.name) ? 1 : (a1.name < a2.name) ? -1 : 0);
     }
-    makeCategory(name){
-        this.categories.push(name);
-        const index = this.categories.indexOf('Uncategorized')
-        this.categories.slice(index);
-        this.categories.sort();
-        this.categories.push('Uncategorized');
+    getAccountIDbyName(accountName) {
+        let accountID = null;
+        for (let account of this.accounts) {
+            if (account.name === accountName) {
+                accountID = account.id;
+                break;
+            }
+        }
+        return accountID;
     }
-}
+    addGroup(name){
+        this.groups.push(name);
+        this.groups.sort();
+    }
+    addCategory(name,groupName,year,month) {
+        const categoryID = uuidv4();
+        this.categories.push(new Category(categoryID,name,groupName,year,month));
+        this.categories.sort((c1, c2) => (c1.name > c2.name) ? 1 : (c1.name < c2.name) ? -1 : 0);
+        this.balances.push(new Balance('assigned',0.00,categoryID,year,month));
+        this.balances.push(new Balance('available',0.00,categoryID,year,month));
+    }
+    getCategoryIDbyName(categoryName) {
+        let categoryID = null;
+        for (let category of this.categories) {
+            if (category.name === categoryName) {
+                categoryID = category.id;
+                break;
+            }
+        }
+        return categoryID;
+    }
+    assignBalance(amount,categoryID,year,month) {
+        let oldAssignedBalance = 0.0;
+        // Updating the assigned balance for this month and year.
+        for (let balance of this.balances) {
+            if (balance.type === 'assigned' && balance.categoryID === categoryID && balance.year === year && balance.month === month) {
+                oldAssignedBalance += balance.amount;
+                balance.amount += amount - oldAssignedBalance;                
+                break;
+            }
+        }
+        // Updating the available balance for this month and year.
+        for (let balance of this.balances) {
+            if (balance.type === 'available' && balance.categoryID === categoryID && balance.year === year && balance.month === month) {
+                balance.amount += amount - oldAssignedBalance;                
+                break;
+            }
+        }
+    }
+    addPayee(name) {
+        let payeeExists = false;
+        for (payee in this.payees) {
+            if (payee === name) {
+                payeeExists = true;
+                break;
+            }
+        }
+        if (!payeeExists) {
+            this.payees.push(name);
+            this.payees.sort();
+        }
+    }
+    addTransaction(amount,payee,year,month,day,accountID,categoryID){
+        this.transactions.push(new Transaction(amount,payee,year,month,day,accountID,categoryID));
+        this.addPayee(payee);
+        // Updating the category's available balance.
+        for(let balance of this.balances){
+            if (balance.type === 'available' && balance.categoryID === categoryID && balance.year === year && balance.month === month) {
+                balance.amount += amount;
+                break;
+            }
+        }
+        // Updating the account's balance.
+        for (let account of this.accounts){
+            if (account.id === accountID){
+                account.balance += amount;
+                break;
+            }
+        }
+    }
+}   
 
 class Account {
     constructor(name,balance,currency){
@@ -39,8 +121,50 @@ class Account {
         this.balance = balance;
         this.currency = currency;
     }
+}   
+
+class Category {
+    constructor(id,name,groupName,year,month) {
+        this.id = id;
+        this.name = name;
+        this.groupName = groupName;
+        this.yearCreated = year;
+        this.monthCreated = month;
+    }
+}
+
+class Balance {
+    constructor(type,amount,categoryID,year,month) {
+        this.type = type; // type = 'assigned', 'active', or 'available'
+        this.amount = amount;
+        this.categoryID = categoryID;
+        this.year = year;
+        this.month = month;
+    }
+}
+
+class Transaction {
+    constructor(amount,payee,year,month,day,accountID,categoryID) {
+        this.id = uuidv4();
+        this.amount = amount;
+        this.payee = payee;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.accountID = accountID;
+        this.categoryID = categoryID;
+    }
 }
 
 export {
     User, Budget, 
 }
+
+
+
+
+/*
+Notes
+
+    - Empty balances must be added whenever a new date is navigated to.
+*/
--- test_activity.js
+++ test_activity.js
@@ -5,23 +5,39 @@ import {User} from "./data_model/classes.js";
 function startActivity() {
     // A new day begins.
     const todaysDate = '2023-11-01';
-    const todaysYear = '2023';
-    const todaysMonth = '11';
+    const todaysYear = 2023;
+    const todaysMonth = 11;
+    const todaysDay = 1;
 
     // A new user is created.
     const anjelove = new User('anjelove','password1234','jelovalera@gmail.com');
    
     // The user starts a new budget.
-    anjelove.makeBudget('Anjelo\'s Budget',todaysDate);
+    anjelove.makeBudget('Anjelo\'s Budget',todaysYear,todaysMonth,todaysDay);
 
     // The user adds their Checkings account.
     const myStartingBalance = 5000.00;
     const currency = 'USD';
-    anjelove.budgets[0].makeAccount('Checkings',myStartingBalance,currency);
+    anjelove.budgets[0].addAccount('Checking',myStartingBalance,currency);
 
-    // The user creates two new categories.
-    anjelove.budgets[0].makeCategory('Essential');
-    anjelove.budgets[0].makeCategory('Nonessential');
+    // The user creates two new groups.
+    anjelove.budgets[0].addGroup('Essential');
+    anjelove.budgets[0].addGroup('Nonessential');
+
+    // The user creates four new categories.
+    anjelove.budgets[0].addCategory('Gas','Essential',todaysYear,todaysMonth);
+    anjelove.budgets[0].addCategory('Food','Essential',todaysYear,todaysMonth);
+    anjelove.budgets[0].addCategory('Cannabis','Nonessential',todaysYear,todaysMonth);
+    anjelove.budgets[0].addCategory('Video Games','Nonessential',todaysYear,todaysMonth);
+
+    // The user assigns money to all categories.
+    anjelove.budgets[0].assignBalance(100.00,anjelove.budgets[0].getCategoryIDbyName('Gas'),todaysYear,todaysMonth);    
+    anjelove.budgets[0].assignBalance(200.00,anjelove.budgets[0].getCategoryIDbyName('Food'),todaysYear,todaysMonth);   
+    anjelove.budgets[0].assignBalance(80.00,anjelove.budgets[0].getCategoryIDbyName('Cannabis'),todaysYear,todaysMonth);    
+    anjelove.budgets[0].assignBalance(70.00,anjelove.budgets[0].getCategoryIDbyName('Video Games'),todaysYear,todaysMonth);
+
+    // The user creates a transaction.
+    anjelove.budgets[0].addTransaction(-45.00,"Curaleaf",todaysYear,todaysMonth,todaysDay,anjelove.budgets[0].getAccountIDbyName('Checking'),anjelove.budgets[0].getCategoryIDbyName('Cannabis'));
 
 
 }
